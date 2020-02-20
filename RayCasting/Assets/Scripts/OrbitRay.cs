094469ce4bc9fa8397daa4bf3290ccee356b3e9e
S00188447 <47103333+S00188447@users.noreply.github.com>
2020-02-20 17:05:16 +0000

    Planet colours working

---
+++ RayCasting/Assets/Scripts/OrbitRay.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitRay : MonoBehaviour
+{
+    public float maxDistance = 100;
+    public LayerMask layerMask;
+    RaycastHit hitResult;
+    Ray mouseRay;
+    Camera myCamera;
+
+
+    public Color selectedColor;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(mouseRay, out hitResult,maxDistance, layerMask))
+            {
+                Vector3 point = hitResult.point;
+                Vector3 normal = hitResult.normal;
+                Vector2 uv = hitResult.textureCoord;
+
+                MeshRenderer mesh = hitResult.collider.gameObject.GetComponent<MeshRenderer>();
+                Material material = mesh.material;
+                Texture2D texture = (Texture2D)material.mainTexture;
+
+                selectedColor = texture.GetPixelBilinear(uv.x, uv.y);
+
+                
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(mouseRay);
+    }
+}
---
+++ RayCasting/Assets/Scripts/WorldResource.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+
+[Serializable]
+public class WorldResource
+{
+    public ResourceTypes Type;
+    public Color ResourceColor;
+
+}
+
+[Serializable]
+    public enum ResourceTypes
+    {
+        Coal,
+        Solid,
+        Water,
+        Gold
+    }
+
+
+
+

ec35e7ebfb419318bb3a08d2e97dacdb160af1b4
S00188447 <47103333+S00188447@users.noreply.github.com>
2020-03-05 17:46:21 +0000

    Scripts updated

---
+++ Navigation Starter Kit/Greybox/Assets/Scripts/NavMeshMover.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public delegate void ReachDestinationDelegate();
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class NavMeshMover : MonoBehaviour
+{
+
+    protected NavMeshAgent agent;
+    public event ReachDestinationDelegate ReachedDestination;
+    public float RangeThreshold = 0.1f;
+
+    public virtual void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();    
+    }
+
+    public virtual void MoveTo(Vector3 position)
+    {
+        agent.SetDestination(position);
+    }
+
+    public virtual void MoveTo(GameObject position)
+    {
+        agent.SetDestination(gameObject.transform.position);
+    }
+
+    public virtual void Stop()
+    {
+        agent.isStopped = true;
+    }
+
+    public virtual void Resume()
+    {
+        agent.isStopped = false;
+    }
+
+
+    public void HasReachedDestination()
+    {
+        if(Vector3.Distance(transform.position, agent.destination) <= RangeThreshold)
+        {
+            //reached the destination
+            if (ReachedDestination != null)
+                ReachedDestination();
+        }
+
+    }
+
+
+    public void Update()
+    {
+        if(agent.pathStatus == NavMeshPathStatus.PathComplete ||
+                agent.pathStatus == NavMeshPathStatus.PathPartial)
+        HasReachedDestination();
+    }
+
+
+}
---
+++ Navigation Starter Kit/Greybox/Assets/Scripts/PlayerMovementAudio.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayerMovementAudio : MonoBehaviour
+{
+    AudioSource audioSource;
+    public List<AudioClip> possibleClips;
+
+
+    public void Start()
+    {     
+        audioSource = GetComponent<audioSource>();
+        GetComponent<PlayerMovementRayCast>().RayCastReady += PlayerNavMover_RayCastReady;
+    }
+
+    private void PlayerNavMover_RayCastReady(RaycastHit selectionData)
+    {
+        if (possibleClips.Count < 0)
+        {
+            //pick random audio
+            int index = Random.Range(0, possibleClips.Count - 1);
+            audioSource.clip = possibleClips[index];
+
+            if (audioSource.isPlaying)
+                audioSource.Play();
+        }     
+    }
+}

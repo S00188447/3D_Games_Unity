f5c1c58de7e6f78be813ad60832112f04079f571
S00188447 <47103333+PoxyZoxy@users.noreply.github.com>
2020-05-14 08:46:40 +0100

    Revert "zipped folder"
    This reverts commit 3cd6de491504d4236a962bd88bbec872b9734419.

---
+++ Ai Navigation/Assets/Scripts/EnemyMoveDirectlyTo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMoveDirectlyTo : NavMeshMover
+{
+    public string tagToTrack = "Player";
+    GameObject trackedPlayer;
+
+    void Start()
+    {
+        trackedPlayer = GameObject.FindGameObjectWithTag(tagToTrack);
+        base.start();    
+    }
+
+
+    void Update()
+    {
+        if(trackedPlayer != null)
+        {
+            MoveTo(trackedPlayer);
+        }
+        
+    }
+}
